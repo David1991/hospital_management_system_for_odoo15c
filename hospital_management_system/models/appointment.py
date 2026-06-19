96b6f24c5b7f781e7922e4412a9b7e648e3c5ed2
Thandar <thandar@Thandars-MacBook-Air.local>
2026-06-19 17:20:48 +0630

    Updating patient.py,appointment.py,patient_view.xml and appointment_view.xml for hospital_management_system

--- hospital_management_system/models/appointment.py
+++ hospital_management_system/models/appointment.py
@@ -1,4 +1,4 @@
-from odoo import models,fields
+from odoo import models,fields,api
 
 class Appointment(models.Model):
     _name = "appointment"
@@ -6,7 +6,37 @@ class Appointment(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
 
     patient_id = fields.Many2one(string='Patient', comodel_name='patient')
+    gender = fields.Selection(related='patient_id.gender', readonly=False)
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
+    ref = fields.Char(string='Reference', help='Reference of the patient from the patient record.')
+    prescription = fields.Html(string='Prescription')
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Low'),
+        ('2', 'High'),
+        ('3', 'Very High'),
+    ], string='Priority')
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('in_consultation', 'In Consultation'),
+        ('done', 'Done'),
+        ('cancelled', 'Cancelled'),
+    ], string='State', required=True, default='draft')
+    doctor_id = fields.Many2one('res.users', string="Doctor")
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
+
+    def action_test(self):
+        print("Button Click !!!!!!!")
+        return {
+                'effect': {
+                    'fadeout': 'slow',
+                    'message': 'Click Successful!',
+                    'type': 'rainbow_man',
+                }
+            }
     
     
--- hospital_management_system/models/patient.py
+++ hospital_management_system/models/patient.py
@@ -1,4 +1,5 @@
-from odoo import models,fields
+from odoo import models,fields,api
+from datetime import date
 
 class Patient(models.Model):
     _name = "patient"
@@ -6,8 +7,21 @@ class Patient(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
 
     name = fields.Char(string='Name', tracking=True)
+    date_of_birth = fields.Date(string='Date of Birth')
     ref = fields.Char(string='Reference', tracking=True)
-    age = fields.Integer(string='Age', tracking=True)
+    age = fields.Integer(string='Age', compute='_compute_age', tracking=True)
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender', tracking=True, default='female')
     active = fields.Boolean(string='Active', default=True)
+    appointment_id = fields.Many2one(string='Appointment', comodel_name='appointment')
+
+    # Computing for Age
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 1
+
     
